2b6d1a62830991b70742cb8fa6ae2b4378bd49bb
Adrian <adrianomucha@gmail.com>
2016-07-29 05:09:33 +0000

    assignment

---
+++ my_database.py
@@ -0,0 +1,11 @@
+{ 'name': 'Adrian Mucha',
+  'dob': '1992-09-10',
+  'notes': 'No additional notes',
+  'details': [
+    { 'type': 'email',
+      'data': 'name.funny@gmail.com'
+    }, {
+      'type': 'phone',
+      'data': '555-555-5555'
+    } ]
+}
